530d0c8ae86b3d92064c36b367b3d443ed861ca7
quisen <quisen@alunos.utfpr.edu.br>
2020-02-14 12:19:24 -0200

    Criação de procedure no Mysql para alterar reservas como inativas

---
+++ sql/ProcedureVerificaReservasAtivas.sql
@@ -0,0 +1,13 @@
+use reserva_sala;
+SET @@global.event_scheduler = 1;
+SET SQL_SAFE_UPDATES = 0;
+delimiter |
+CREATE EVENT verificar_reservas_ativas
+    ON SCHEDULE
+        EVERY 10 MINUTE
+        STARTS CURDATE()
+        ON COMPLETION PRESERVE ENABLE DO
+            BEGIN
+				UPDATE reserva_sala.reserva SET ativo = 0 WHERE data_hora_fim < NOW();
+            END 
+| delimiter ;
